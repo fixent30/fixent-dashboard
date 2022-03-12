d187d4cec6b793553c454b5ed15da47d482a777e
Arindam242 <creativeab26@gmail.com>
2022-03-12 18:04:29 +0530

    add edit adn delete button  in computer price

--- src/ComputerPrcie.js
+++ src/ComputerPrcie.js
@@ -1,18 +1,25 @@
 import {
   Create,
   Datagrid,
+  DeleteButton,
   Edit,
+  EditButton,
   List,
   SimpleForm,
   TextField,
   TextInput,
 } from 'react-admin'
 
+const MyEditButton = (props) => <EditButton {...props} />
+const MyDeleteButton = (props) => <DeleteButton {...props} />
+
 export const ComputerPriceList = (props) => (
   <List {...props}>
     <Datagrid>
       <TextField source="price" />
       <TextField source="speces" />
+      <MyEditButton />
+      <MyDeleteButton />
     </Datagrid>
   </List>
 )
